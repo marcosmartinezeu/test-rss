38cfc48a8d1d131f3a04534aaaad718398902054
Marcos Martínez <marcos.martinez.muniz@gmail.com>
2019-05-17 18:23:56 +0200

    Add command tests

--- tests/Command/MeneameCommandsTest.php
+++ tests/Command/MeneameCommandsTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests\Command;
 
+use App\Entity\Rss;
 use App\Exception\RssStatusNotValid;
 use App\Repository\RssRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class MeneameCommandsTest extends KernelTestCase
@@ -39,6 +42,54 @@ class MeneameCommandsTest extends KernelTestCase
         $commandTester->execute([
             'command' => $command->getName(),
             '--status' => 'NotValidStatus'
+        ]);
+    }
+
+    public function testListMainCommand()
+    {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('meneame:main-list');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName()
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('results', $output);
+    }
+
+    public function testQueuedListCommand()
+    {
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('meneame:queued-list');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName()
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('results', $output);
+    }
+
+    public function testListCommandFailMaxResultArgument()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('meneame:main-list');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            '--max' => 'four'
         ]);
     }
 
